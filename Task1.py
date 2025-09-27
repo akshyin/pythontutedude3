47b03d5d869207479d0848b79d7502ae7b164414
akshyin <akshay.akshyin@gmail.com>
2025-09-27 10:13:02 +0530

    addition of comments

--- Task1.py
+++ Task1.py
@@ -1,18 +1,27 @@
 def factorial(n):
+    #intailising final value with 1 as factorial is found using multiplication and it should start with 1
     fac=1
+    #if n is negative, as mathematical function it should return that factorial is not defined for negative number
     if n<0:
         raise "factorial is not defined for negative numbers";
+    # if n is 0 or 1, value of factorial would be 1
     if n<2 and n>=0:
-        return 1
+        return fac
+    #if n>=2 then factorial(n)=factorial(n-1)*n so expansion of the for loop would be fac=1*2*3 for n 3
     for i in range(1,n+1):
         fac=fac*i
     return fac
 
 if __name__ == "__main__":
+    #Asks user for the input the number
     n=input("Enter a number: ")
     try:
+        #user may accidently use a value with is not convertable to integer
         n=int(n)
+        #factorial function will take care if n is passed as whole number or not as it would give error
+        # and error will not execute next line but go to except section and would print error statement provided
         fac=factorial(n)
         print(f"Factorial of {n} is {fac}");
     except:
+        #in case of any error, as we know factorial is valid for whole number so any error would lead to this statement
         print("factorial can be only for whole number")
--- Task2.py
+++ Task2.py
@@ -2,17 +2,25 @@ import math
 
 
 if __name__ == "__main__":
+    #takes input from user
     n=input("Enter a number ")
     try:
+        #try to convert it in float, as integer input for given  mathematical operation, float is appropriate
         n=float(n)
+        #As we know, square root can be for only non-negative numbers
         if n>=0:
             print(f"square root: {math.sqrt(n)}")
+        
         else:
+            #incase of n is given negative
             print(f"square root is not defined for {n}")
+        #As log domain is only non zero  positive number
         if n>0:
             print(f"Logarithm: {math.log(n,math.e)}")
         else:
             print(f"Logarithm is not defined for negative numbers or zero")
+        #prints the value of sin, n is treated as given in radian
         print(f"Sine: {math.sin(n)}")
     except:
+        #Any error should provide the reason of error, error may be appropriatly provided in this context. 
         print("mathematical function can be executed for integer or floating point numbers only")
